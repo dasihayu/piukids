5f882c05b078ce8ffe9348631493fcc9e710c236
YordanAseramaLuturyali <yordanluturyali@gmail.com>
2023-03-13 22:07:38 +0700

    membuat tampilan artikel dan kategori

---
+++ resources/views/template/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="nav flex w-full h-14 border-b-2 items-center px-4" id="nav">
+    <a href="\">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+        viewBox="0 0 24 24">
+        <path fill="currentColor"
+            d="M15.125 21.1L6.7 12.7q-.15-.15-.212-.325q-.063-.175-.063-.375t.063-.375q.062-.175.212-.325l8.425-8.425q.35-.35.875-.35t.9.375q.375.375.375.875t-.375.875L9.55 12l7.35 7.35q.35.35.35.862q0 .513-.375.888t-.875.375q-.5 0-.875-.375Z" />
+        </svg>
+    </a>
+    <h1 class=" ml-6 font-semibold ">Artikel PIUKIDS</h1>
+</nav>
